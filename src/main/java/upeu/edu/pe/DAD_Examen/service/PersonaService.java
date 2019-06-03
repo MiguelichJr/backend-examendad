3361368d52e1540b9464c3a0cdac11ebcdaf70df
MiguelichJr <owenmejia@upeu.edu.pe>
2019-06-03 10:39:07 -0500

    commit github

---
+++ src/main/java/upeu/edu/pe/DAD_Examen/service/PersonaService.java
@@ -0,0 +1,11 @@
+package upeu.edu.pe.DAD_Examen.service;
+
+import java.util.List;
+
+
+import upeu.edu.pe.DAD_Examen.dto.Persona;
+
+public interface PersonaService {
+	List<Persona> listarpersona();
+	List<Persona> buscarPersonaporDni(int dni);
+}
